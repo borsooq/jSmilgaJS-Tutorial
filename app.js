b08cba8f1ba97f75d4eba49ab19e98c538050900
Mirko Boruc <mkbu@demant.com>
2022-03-29 20:39:53 +0200

    console alert and document write exercises

---
+++ app.js
@@ -0,0 +1,8 @@
+var myElement = document.getElementsByClassName("myClass");
+
+document.write({ name: "hello" });
+
+// alert("hello world");
+
+console.log({ name: "hello" });
+console.log(myElement);
