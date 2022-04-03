d3720738daf9eb0b79e95d9541e6b485aef90502
Mirko Boruc <mkbu@demant.com>
2022-04-03 16:54:25 +0200

    third challenge

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-let street = "street";
-let country = "country";
+const score1 = 1;
+const score2 = 10;
+const score3 = 100;
 
-let fullMailingAddress = street + " " + country;
+const totalScore = score1 + score2 + score3;
+const average = totalScore / 3;
 
-console.log(fullMailingAddress);
+console.log(totalScore);
+console.log(average);
