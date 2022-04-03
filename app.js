72faf1e7c483da2836c87bb260fa85846f5d85d7
Mirko Boruc <mkbu@demant.com>
2022-04-03 18:41:47 +0200

    while loops

--- app.js
+++ app.js
@@ -1,40 +1,11 @@
-const person1 = {
-  name: "kamil",
-  age: 20,
-  status: "resident",
-};
+let amount = 10;
 
-const person2 = {
-  name: "mamil",
-  age: 50,
-  status: "tourist",
-};
-
-if (person1.age >= 18 && person1.status === "resident") {
-  console.log("you can cast a vote");
-} else {
-  console.log("you're not elegible");
+while (amount > 0) {
+  console.log(`amount id => ${amount}`);
+  amount--;
 }
 
-const dice = 1;
-
-switch (dice) {
-  case 1:
-    console.log(`the value is ${dice}`);
-    break;
-  case 2:
-    console.log(`the value is ${dice}`);
-    break;
-  case 3:
-    console.log(`the value is ${dice}`);
-    break;
-  case 4:
-    console.log(`the value is ${dice}`);
-    break;
-  case 5:
-    console.log(`the value is ${dice}`);
-    break;
-  default:
-    console.log(`the value is ${dice}`);
-    break;
-}
+do {
+  console.log(`do amount id => ${amount}`);
+  amount--;
+} while (amount > 0);
