b7c00a9af54790c30c051ddcb8fc99eb12069826
Mirko Boruc <mkbu@demant.com>
2022-04-09 16:04:24 +0200

    strings challenge

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
-let text = " Peter Jordan";
+function fullName({ firstName, lastName }) {
+  return (firstName + lastName).toUpperCase();
+}
 
-let result = text.length;
+person = {
+  lastName: "boruc",
+  firstName: "kamil",
+};
 
-console.log(result);
-
-console.log(text.length);
-
-console.log(text.toLowerCase());
-console.log(text.toUpperCase());
-console.log(text.charAt(0));
-console.log(text.charAt(12));
-console.log(text.charAt(text.length - 1));
-console.log(text.indexOf("p"));
-console.log(text.trim());
-console.log(text.startsWith(" Peter"));
-console.log(text.trim().toLocaleLowerCase().startsWith("peter"));
-console.log(text.includes("ord"));
-console.log(text.slice(0, 5));
-console.log(text.slice(-5));
-
-// const person = {
-//   name: "peter",
-//   greeting() {
-//     console.log("hey, i am peter");
-//   },
-// };
-
-// console.log(person);
+console.log(fullName(person));
