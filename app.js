bf0a1ea7e424acd46356dd308de43edb37a9cbfa
Mirko Boruc <mkbu@demant.com>
2022-04-09 15:50:43 +0200

    strings

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-let i;
-for (i = 0; i < 10; i++) {
-  console.log(`value is => ${i}`);
-}
+let text = " Peter Jordan";
+
+let result = text.length;
+
+console.log(result);
+
+console.log(text.length);
+
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+console.log(text.charAt(0));
+console.log(text.charAt(12));
+console.log(text.charAt(text.length - 1));
+console.log(text.indexOf("p"));
+console.log(text.trim());
+console.log(text.startsWith(" Peter"));
+console.log(text.trim().toLocaleLowerCase().startsWith("peter"));
+console.log(text.includes("ord"));
+console.log(text.slice(0, 5));
+console.log(text.slice(-5));
+
+// const person = {
+//   name: "peter",
+//   greeting() {
+//     console.log("hey, i am peter");
+//   },
+// };
+
+// console.log(person);
