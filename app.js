e203cf9256021f0a978ae2aa7c54d0ef30082f37
Mirko Boruc <mkbu@demant.com>
2022-04-10 18:23:56 +0200

    math

---
+++ app.js
@@ -0,0 +1,19 @@
+// const number = 4.56789;
+// const result = Math.floor(number);
+
+// const number = 4.56789;
+// const result = Math.ceil(number);
+
+// const number = 89;
+// const result = Math.sqrt(number);
+
+const number = 89;
+//const result = Math.sqrt(number);
+
+//console.log(result);
+console.log(Math.PI);
+console.log(Math.min(5, 6, 7, 1, 2));
+console.log(Math.max(5, 6, 7, 1, 2));
+
+const result = Math.floor(Math.random() * 10 + 1);
+console.log(result);
