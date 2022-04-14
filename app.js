d1916405063ea3934acdb361b592aaf787df2f48
Mirko Boruc <mkbu@demant.com>
2022-04-14 20:53:14 +0200

    insertBefore

--- app.js
+++ app.js
@@ -7,18 +7,19 @@ const bodyDiv = document.createElement("div");
 //create text node
 
 const text = document.createTextNode("a simple body div");
+const heading1 = document.querySelector("red");
 
 bodyDiv.appendChild(text);
-
-document.body.appendChild(bodyDiv);
 
 const heading = document.createElement("h2");
 const headingText = document.createTextNode("dynamic heading");
 
 heading.appendChild(headingText);
 
+document.body.insertBefore(bodyDiv, result);
+
 heading.classList.add("blue");
 
-result.appendChild(heading);
+document.body.insertBefore(heading, heading1);
 
 console.log(result.children);
