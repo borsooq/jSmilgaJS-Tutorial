1e861e66f83d3f86a4431d03989ddfa97e75e4a1
Mirko Boruc <mkbu@demant.com>
2022-05-08 12:35:35 +0200

    modules2

---
+++ app_modules.js
@@ -0,0 +1,12 @@
+import { random, people } from "./utils/data.js";
+import showPeople from "./utils/showPeople.js";
+import get from "./utils/getElement.js";
+
+console.log(random);
+
+const container = get(".container");
+const btn = get(".btn");
+
+btn.addEventListener("click", () => {
+  container.innerHTML = showPeople(people);
+});
